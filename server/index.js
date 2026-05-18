d9baee70d33722001e8dbc7a9c2652c4f241c4f2
Kshitij Sandelya <kshitijsandelya1607@gmail.com>
2026-05-19 00:47:39 +0530

    chore: update cors to support production frontend url

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ app.use(express.json());
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
